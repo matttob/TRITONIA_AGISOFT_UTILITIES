d1a59c439d1bb6d73d1cc15a866f9d1a98de703e
karlroberts0401 <93919314+karlroberts0401@users.noreply.github.com>
2022-05-13 12:10:47 +0100

    Add files via upload

---
+++ GRADUAL_SELECTION/PROJACC_GRADUALSELECTION.py
@@ -0,0 +1,54 @@
+import Metashape
+import sys
+import inspect
+import numpy as np  
+import matplotlib.pyplot as plt
+
+# Define target removed points percentage 
+target_percentage = 0.499
+# define number of projacc error iteration steps
+number_of_err_steps = 100
+
+
+# loop through all of the available chunks in the model
+for chunk in Metashape.app.document.chunks:
+    # save text by defining filter syntax
+    f = Metashape.PointCloud.Filter()
+    # intitialise Projection Accuracy filter
+    f.init(chunk, Metashape.PointCloud.Filter.ProjectionAccuracy)
+    # create numpy array of values of Projection Accuracy using the min and max value provided by the filter
+    projacc_iter = np.arange(f.min_value, f.max_value,((f.max_value-f.min_value)/number_of_err_steps))
+    
+    #initialise list for storing values of the percentage of points selected for each value of Projection Accuracy
+    percent_selecteds =[]
+    
+
+    # loop through all values of Projection Accuracy and store the number of points selected in each case
+    for i in projacc_iter:
+        # select points 
+        f.selectPoints(i)
+        # compute the number of points selected
+        nselected = len([p for p in chunk.point_cloud.points if p.selected])
+        # compute percentage of points selected from total number of points in chunk
+        percent_selected = nselected / len(chunk.point_cloud.points)
+        # create list of percentage selected values
+        percent_selecteds.append(percent_selected)
+    
+    # change list to numpy array
+    percent_selecteds = np.array(percent_selecteds)
+    # apply same selection criteria to projacc_iter array to ensure correct indexing
+    projacc_iter_less_than_half = projacc_iter[percent_selecteds<target_percentage]
+    # select only values of percent selected that are less than our target percentage
+    percent_selecteds_less_than_half = percent_selecteds[percent_selecteds<target_percentage]
+    
+    # find absolute difference between percentage of points selected and target percentage
+    percent_selected_diff_to_target = abs(percent_selecteds_less_than_half - target_percentage) 
+    
+    # find index of smallest : closest value to target
+    rep_cunc_half_points_selected_ind = np.argmin(percent_selected_diff_to_target)
+
+    # remove points using the projacc_iter value with the index defined above
+    f.removePoints(projacc_iter_less_than_half[rep_cunc_half_points_selected_ind])
+
+    # print(percent_selecteds_less_than_half[rep_cunc_half_points_selected_ind])
+    print("Percentage of total points removed based on Projection Accuracy = " + str((percent_selecteds_less_than_half[rep_cunc_half_points_selected_ind])*100) + "%")
