e90d0654b1a6ba6b9f9a57f57d89b9dd084ecf56
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-26 11:16:48 +0100

    Changing image/quality from 0.73 to 0.5

--- DISABLE_LOW_QUALITY_IMAGES.py
+++ DISABLE_LOW_QUALITY_IMAGES.py
@@ -5,6 +5,6 @@ import Metashape
 for chunk in Metashape.app.document.chunks:
     chunk.analyzePhotos(chunk.cameras)
     for camera in chunk.cameras:
-        if float(camera.meta["Image/Quality"]) < 0.73:
+        if float(camera.meta["Image/Quality"]) < 0.5:
             camera.enabled = False
 
