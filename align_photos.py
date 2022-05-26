0cef52636fed6b44ac97e9cdb7382772886cd82e
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-26 16:41:55 +0100

    Adding buildDepthmaps

--- align_photos.py
+++ align_photos.py
@@ -5,4 +5,6 @@ for chunk in Metashape.app.document.chunks:
     chunk.matchPhotos(keypoint_limit = 40000, tiepoint_limit = 10000, generic_preselection = True, reference_preselection = True)
 
     chunk.alignCameras()
+
+    chunk.buildDepthMaps(downscale = 2, filter_mode = Metashape.MildFiltering) #DOWNSCALE: Ultra = 1, High = 2, Medium = 4, Low = 8, Lowest = 16
    
