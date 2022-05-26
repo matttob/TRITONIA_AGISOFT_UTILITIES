e2642b435e68f4657bc78478b377494a38c80642
karlroberts0401 <93919314+karlroberts0401@users.noreply.github.com>
2022-05-26 14:48:35 +0100

    Delete FILTER_CONFIDENCE directory

--- FILTER_CONFIDENCE/densecloud_confidence_filter.py
+++
@@ -1,9 +0,0 @@
-import Metashape
-
-for chunk in Metashape.app.document.chunks:
-
-    chunk.dense_cloud.setConfidenceFilter(0,2) #shows only dense points with confidence between 0 and 2
-    chunk.dense_cloud.removePoints(list(range(128))) #removes all "visible" points of the dense cloud
-    chunk.dense_cloud.resetFilters() #resets filters to show remaining points
-
-
