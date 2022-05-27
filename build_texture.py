9567fbb653e9a152dbe4c32b1f294f2f08c3fa5e
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-27 16:12:13 +0100

    Adding build uv

--- build_texture.py
+++ build_texture.py
@@ -1,6 +1,7 @@
 import Metashape
 
-
 for chunk in Metashape.app.document.chunks:
 
-    chunk.buildTexture(blending_mode=Metashape.MosaicBlending, texture_size=16384, fill_holes=True, ghosting_filter=True, texture_type=Metashape.DiffuseMap, transfer_texture=True  ) 
+    chunk.buildUV(page_count=1, texture_size=8192)
+
+    chunk.buildTexture(blending_mode=Metashape.MosaicBlending, texture_size=16384, fill_holes=True, ghosting_filter=True, texture_type = Metashape.Model.TextureType.DiffuseMap, transfer_texture=True  ) 
