9085517c14cbe55ed65583a3faeb02263a8dd706
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-30 11:42:36 +0100

    Remove custom face count

--- build_mesh.py
+++ build_mesh.py
@@ -2,7 +2,5 @@ import Metashape
 
 for chunk in Metashape.app.document.chunks:
 
-
-    chunk.buildModel(surface_type=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation, face_count=Metashape.HighFaceCount,
-    face_count_custom=200000, source_data=Metashape.DenseCloudData, vertex_colors=True,
-    vertex_confidence=True, volumetric_masks=False, keep_depth=True, subdivide_task=True)
+    chunk.buildModel(surface_type=Metashape.Arbitrary, interpolation=Metashape.EnabledInterpolation, face_count=Metashape.HighFaceCount, source_data=Metashape.DenseCloudData, vertex_colors=True,
+    vertex_confidence=True, volumetric_masks=False, keep_depth=False, subdivide_task=True)
