9a2ac6d0e84aa9ccb005452b9c6c98266536b73c
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-30 11:35:33 +0100

    Add buildDepthmaps

--- build_dense_cloud.py
+++ build_dense_cloud.py
@@ -1,6 +1,10 @@
 import Metashape
 
-#densecloud,
 for chunk in Metashape.app.document.chunks:
 
-    chunk.buildDenseCloud(quality = Metashape.HighQuality,point_colors=True, point_confidence=True, keep_depth=True, max_neighbors=100, subdivide_task=True, filter = Metashape.ModerateFiltering)
+    #Downscale quality scale: Ultra High = 1, High = 2, Medium = 4, Low = 8, Lowest = 16
+    chunk.buildDepthMaps(downscale = 2, filter_mode = Metashape.ModerateFiltering)
+
+    chunk.buildDenseCloud(point_colors=True, point_confidence=False, keep_depth=True, max_neighbors=100, subdivide_task=True, workitem_size_cameras=20, max_workgroup_size=100)
+
+    
