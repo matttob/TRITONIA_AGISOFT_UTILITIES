b58591019d5df141f51fb0973ffdb954ffc45e3e
karlroberts0401 <karlroberts0401@gmail.com>
2022-05-30 11:12:52 +0100

    Add downscale quality scale guide

--- align_photos.py
+++ align_photos.py
@@ -2,7 +2,8 @@ import Metashape
 
 for chunk in Metashape.app.document.chunks:
     
-    chunk.matchPhotos(downscale = 2, generic_preselection=False, reference_preselection=False, filter_mask=False, mask_tiepoints=True, keypoint_limit=40000, tiepoint_limit=4000, guided_matching=False)
+    #Downscale quality scale: Highest = 0, High = 1, Medium = 2, Low = 4, Lowest = 8
+    chunk.matchPhotos(downscale = 2, generic_preselection=False, reference_preselection=False, filter_mask=False, mask_tiepoints=True, keypoint_limit=40000, tiepoint_limit=4000, guided_matching=False) 
 
     chunk.alignCameras(adaptive_fitting=False)
 
