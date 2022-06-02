9042fde7bb64dae474878818691a46e10d232aa0
karlroberts0401 <karlroberts0401@gmail.com>
2022-06-02 14:23:11 +0100

    Add comments

--- build_texture.py
+++ build_texture.py
@@ -1,7 +1,7 @@
 import Metashape
-
+#for all chunks in the project 
 for chunk in Metashape.app.document.chunks:
-
+    #building texture
     chunk.buildUV(mapping_mode=Metashape.GenericMapping, page_count=1, texture_size=8192)
 
     chunk.buildTexture(blending_mode=Metashape.MosaicBlending, texture_size=16384, fill_holes=True, ghosting_filter=True, texture_type = Metashape.Model.TextureType.DiffuseMap, transfer_texture=True  ) 
