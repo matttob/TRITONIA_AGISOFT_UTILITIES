be71d3b884bad527b0d37dfecd85ea5b2dda1e77
matttob <matttoberman@hotmail.com>
2022-06-08 13:30:40 +0100

    fix errors in chunk naming

--- import_images_from_folders_metashape.py
+++ import_images_from_folders_metashape.py
@@ -1,6 +1,6 @@
 import Metashape
 from pathlib import Path
-
+import os
 # create list of image file names
 
 # Enter path of highest tier of video files directory structure
@@ -10,18 +10,25 @@ video_files_highest_directory=Path(video_files_highest_directory)
 # Search in all sub directories for any files with .mpg extension and create list of full file paths for each video file
 video_file_paths = [str(pp) for pp in video_files_highest_directory.glob("**/*.mpg")]
 
-# loop thogh each directoty that contains a video file and find the 'FRAMES' directory  within that make a list of the full path names of each image and imort into agisoft
-for image_directories in video_file_paths:
-    image_file_path = image_directories.split('\\')
-    cropped_image_folder = ('/'.join(map(str, image_file_path[0:-1])))
-    image_direcroty = ([ f.path for f in os.scandir(cropped_image_folder) if f.is_dir() ])
-    image_direcroty = Path(image_direcroty[0])
-    image_file_paths = [str(pp) for pp in image_direcroty.glob("**/*.jpg")]
 
+# loop thogh each directoty that contains a video file and find any sub direcrorires directory  within that make a list of the full path names of each image and imort into agisoft
+for video_file_names in video_file_paths:
+    split_video_file_path = video_file_names.split('\\')
 
-    doc = Metashape.app.document
-    chunk = doc.addChunk()
-    chunk.label = image_file_path[-1]
+    directory_containing_video_files = ('/'.join(map(str, split_video_file_path[0:-1])))
+    
+    
+    image_direcroties = ([ f.path for f in os.scandir(directory_containing_video_files) if f.is_dir() ])
+    # loop through all subdirectories
+    for video_file_sub_names in image_direcroties:
+        image_direcroty = Path(video_file_sub_names)
+        # print(image_direcroty)
+        image_file_paths = [str(pp) for pp in image_direcroty.glob("**/*.jpg")]
 
-    chunk.addPhotos(image_file_paths)
-    Metashape.app.update()  
+        
+        doc = Metashape.app.document
+        chunk = doc.addChunk()
+        chunk.label = video_file_sub_names.split('\\')[-1]
+
+        chunk.addPhotos(image_file_paths)
+        Metashape.app.update()  
