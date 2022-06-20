93d870df27abfbf92209334e5f791e5220cd6f33
karlroberts0401 <karlroberts0401@gmail.com>
2022-06-20 16:06:34 +0100

    Change confidence level

--- densecloud_confidence_filter.py
+++ densecloud_confidence_filter.py
@@ -2,7 +2,7 @@ import Metashape
 #for all chunks in the project 
 for chunk in Metashape.app.document.chunks:
 
-    chunk.dense_cloud.setConfidenceFilter(0,2) #shows only dense points with confidence between 0 and 2
+    chunk.dense_cloud.setConfidenceFilter(0,1) #shows only dense points with confidence between 0 and 2
     chunk.dense_cloud.removePoints(list(range(128))) #removes all "visible" points of the dense cloud
     chunk.dense_cloud.resetFilters() #resets filters to show remaining points
 
